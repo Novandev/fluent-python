62128395151b8d37552b54d3360b50a871844edd
novandev <novanadams35@gmail.com>
2019-10-16 21:20:53 -0700

    added more to the chard deck, starting next parts

--- chapter1/french-deck.py
+++ chapter1/french-deck.py
@@ -1,13 +1,23 @@
-import collection
+import collections
+from random import choice,shuffle
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')  # This should be a list of numers 1-11 as well as the non-number cars
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')  # This should be a list of numers 1-11 as well as the face carsd
     suits = 'spades diamonds clubs hearts'.split() # Spliting this string based onthe names of the suits
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks] #for everyone one of the sutes, create all cards 1-11 and the face cards
     def __len__(self):
         return len(self._cards)
     def __getitem__(self, position):
         return self._cards[position]
 
+
 if __name__ == "__main__":
+    #  test_card = Card('3', 'clubs')
+    #  print(test_card)
+
+     test_deck = FrenchDeck()
+     print(len(test_deck))
+     print(test_deck[1])
+     print(choice(test_deck))
+     print(test_deck[1:10])
