5ee19a86ef67732c58a416dfd00a620202505294
novandev <novanadams35@gmail.com>
2020-05-17 20:31:56 -0700

    add/ generator need to check is works

--- chapter2/generator.py
+++ chapter2/generator.py
@@ -7,6 +7,16 @@ symbols = '!@#$%^'
 # This will initialize a tuple with all of the values in the sybpl as ordinals
 print(tuple(ord(symbol) for symbol in symbols))
 
+#  The array contructor takes 2 arguments the first defines a storage type
 tuple_ordinal = array.array('I', (ord(symbol) for symbol in symbols))
 
 print(tuple_ordinal)
+
+# Let's use a generator to print tshirts and sizes
+
+colors = ['Red', 'Blue']
+
+sizes = ['S', 'M', 'L']
+
+for shirt in (f'{color}, {size}' for color in colors for size in sizes):
+    print(shirt)
